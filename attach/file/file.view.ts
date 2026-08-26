e94738b32bc4e1aa95729218e504cf65210beca1
thenesterov <thenesterov@proton.me>
2026-08-26 21:15:04 +0500

    $iva_attach: add module for uploading files

---
+++ attach/file/file.view.ts
@@ -0,0 +1,31 @@
+namespace $.$$ {
+	export class $iva_attach_file extends $.$iva_attach_file {
+		@ $mol_mem
+		file(): File {
+			return this.files().at(0);
+		}
+
+		@ $mol_mem
+		title(): string {
+			const filename = this.file()?.name ?? "";
+			return this.cut_string(filename, this.title_max_length());
+		}
+
+		@ $mol_mem
+		tooltip(): string {
+			return this.file()?.name ?? "...";
+		}
+
+		cut_string(str: string, max_length: number): string {
+			if (str.length <= max_length) {
+				return str;
+			}
+
+			const keep = Math.floor((max_length - 3) / 2);
+			const start = str.substring(0, keep);
+			const end = str.substring(str.length - keep);
+
+			return `${start}...${end}`;
+		}
+	}
+}
